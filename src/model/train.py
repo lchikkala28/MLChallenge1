f20a9acfbf70a93c462312485b4566afbe794470
lchikkala28 <lokeshchikkala.cas02@outlook.com>
2024-07-10 00:41:42 +0530

    fix the length

--- src/model/train.py
+++ src/model/train.py
@@ -39,7 +39,9 @@ def get_csvs_df(path):
 def split_data(df, test_size=0.2):
     X = df.drop("Diabetic", axis=1)
     y = df["Diabetic"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size
+        )
     return X_train, X_test, y_train, y_test
 
 
